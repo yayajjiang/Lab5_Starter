a70519a7c48b0b957ab505912963392278fc80b4
jinya jiang <60903767+yayajjiang@users.noreply.github.com>
2022-10-31 00:45:22 -0700

    add comments and figured out two bugs
     new jsConfetti(); -> new JSConfetti();

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -8,22 +8,29 @@ function init() {
   const textToVoice = document.getElementById('text-to-speak');
   const voiceSelect = document.getElementById('voice-select');
   const button = document.querySelector('button');
+  let voices = []
 
-
-  synth.addEventListener('voiceschanged', (event) => {
+  synth.addEventListener('voiceschanged', function() {
     //get the voice list
-    let voices = synth.getVoices();
+    voices = synth.getVoices();
+    //iterate through the list
     for (let i = 0; i < voices.length; i++) {
       const option = document.createElement('option');
-      option.textContent = voices[i].name + "(" + voices[i].lang + ")";
+      option.textContent = `${voices[i].name} (${voices[i].lang})`;
+  
       option.setAttribute('data-lang', voices[i].lang);
       option.setAttribute('data-name', voices[i].name);
       voiceSelect.appendChild(option);
     }
   })
-  //when the button is pressed
+  // populateVoiceList();
+  // if (speechSynthesis.onvoiceschanged !== undefined) {
+  //   speechSynthesis.onvoiceschanged = populateVoiceList;
+  // }
+
+  //when the button is pressed, speak and change the images
   button.addEventListener('click', function() {
-    faceImg.src = "assets/images/smiling-open.png";
+    //faceImg.src = "assets/images/smiling-open.png";
     const utterThis = new SpeechSynthesisUtterance(textToVoice.value);
     const selectedOption = voiceSelect.selectedOptions[0].getAttribute('data-name');
     let voices = synth.getVoices();
@@ -32,11 +39,15 @@ function init() {
         utterThis.voice = voices[i];
       }
     }
+
     synth.speak(utterThis);
 
-    utterThis.addEventListener('end', function(){
-      faceImg.src = "assets/images/smiling.png";
-    })
+    // utterThis.addEventListener('end', function(){
+    //   faceImg.src = "assets/images/smiling.png";
+    // })
+    //change the faces in the start and end
+    utterThis.addEventListener('start',() => faceImg.src = "assets/images/smiling-open.png");
+    utterThis.addEventListener('end',() => faceImg.src = "assets/images/smiling.png");
   }) 
 
 }
--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -1,5 +1,3 @@
-// expose.js
-
 
 window.addEventListener('DOMContentLoaded', init);
 //already imported import JSConfetti from 'js-confetti'
@@ -9,58 +7,62 @@ function init() {
 
   //const expose = document.getElementById('expose');
   const horns = document.getElementById('horn-select');
-  const sound = document.querySelector('button');
-  
   //get the image by alternate text of an image
-  const image = document.querySelector("[alt='No image selected']");
+  const ig = document.querySelector("[alt='No image selected']");
   //get the name of horn, option value
   //const hornName = horns.value;
-  //get audio element
+  // //get audio element
   const audio = document.querySelector('audio');
-  const volume = document.querySelector("[type='range']");
+  const button = document.querySelector('button');
+  
+  const v = document.querySelector("[type='range']");
   const volumeImg = document.querySelector("[src='assets/icons/volume-level-2.svg']");
-  const jsConfetti = new jsConfetti();
+  const Confetti = new JSConfetti();
 
   //change the pictures and audio for every according option
-  horns.addEventListener('change', function() {
+  horns.addEventListener('change', function(){
+    //change the image and audio accordingly
     if(horns.value == "air-horn"){
-      image.src = "assets/images/air-horn.svg";
+      ig.src = "assets/images/air-horn.svg";
       audio.src = "assets/audio/air-horn.mp3";
     }else if(horns.value == "car-horn"){
-      image.src = "assets/images/car-horn.svg";
+      ig.src = "assets/images/car-horn.svg";
       audio.src = "assets/audio/car-horn.mp3";
     }else if(horns.value == "party-horn"){
-      image.src = "assets/images/party-horn.svg";
+      ig.src = "assets/images/party-horn.svg";
       audio.src = "assets/audio/party-horn.mp3";
-    }else{}
+    }
     
-  });
+  })
   
   
-  volume.addEventListener('change', function(){
-    audio.volume = volume.value / 100;
-    if (volume.value == 0) {
+  v.addEventListener('change', function(){
+    audio.volume = v.value / 100;
+    //change the icon accordingly
+    if (v.value == 0) {
       //volumeImg.alt = "Volume level 0";
       volumeImg.src = "assets/icons/volume-level-0.svg";
-    } else if (volume.value < 33) {
+    } else if (v.value < 33) {
       //volumeImg.alt = "Volume level 1";
       volumeImg.src = "assets/icons/volume-level-1.svg";
-    } else if (volume.value < 67) {
+    } else if (v.value < 67) {
       //volumeImg.alt = "Volume level 2";
       volumeImg.src = "assets/icons/volume-level-2.svg";
-    } else if (volume.value <= 100) {
+    } else if (v.value <= 100) {
       //volumeImg.alt = "Volume level 3";
       volumeImg.src = "assets/icons/volume-level-3.svg";
-    } else{}
-  });
-  
-  sound.addEventListener('click', function() {
-    if (horns.value == "party-horn") {
-      jsConfetti.addConfetti();
     }
-    //play the audio
-    audio.play();
-  });
+  })
+  
+  button.addEventListener('click', function() {
+    //audio.load();
+    audio.play(); //play the audio
+    if (horns.value == "party-horn"){
+      Confetti.addConfetti();
+    }
+    
+    
+  })
   
 
 }
